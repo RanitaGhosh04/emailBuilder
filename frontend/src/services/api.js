ef585de538b476954e0982e0aa001151315666b5
Ranita Ghosh <82662107+RanitaGhosh04@users.noreply.github.com>
2025-01-22 21:29:47 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://emailbuilderback.onrender.com';
+const API_BASE_URL = 'https://emailbuilderback.onrender.com/api/getemailLayout';
 
 export const emailService = {
   getEmailLayout: async () => {
